379b6aa6552662cfbad8f8d9c379caa66031c68d
reynold-yoon <reynold.yoon@kakaocorp.com>
2023-05-17 20:25:05 +0900

    커밋 테스트

---
+++ src/main/java/me/whiteship/designpatterns/review_20230518/_15_interpreter/_01_before/App.java
@@ -0,0 +1,7 @@
+package me.whiteship.designpatterns.review_20230518._15_interpreter._01_before;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("commit test");
+    }
+}
